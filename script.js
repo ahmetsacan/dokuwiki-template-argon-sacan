f9ed274f48418279ecf8803e88e784e125c4243a
ahmetsacan <ahmetdevel@umich.edu>
2024-08-06 13:23:04 -0400

    Fixed .perfectScrollbar() is not a function error.

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
+/* DOKUWIKI:include flatpickr.js */
+/* DOKUWIKI:include perfect-scrollbar.js */
 /* DOKUWIKI:include assets/js/core/bootstrap.min.js */
 /* DOKUWIKI:include assets/js/argon-design-system.min.js */
